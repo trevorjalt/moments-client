764367b374f58c80cbb0f74abe44fa81d9cb4987
Trevor J Alt <trevor@trevorjalt.com>
2021-02-19 16:47:48 -0500

    congif adjustment

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
-export default {
+
+const endpoints = {
     API_ENDPOINT: (process.env.REACT_APP_ENV === 'production')
         ? 'https://moments-app-api.herokuapp.com/api'
         : 'http://localhost:8000/api',
     API_TOKEN: process.env.REACT_APP_API_TOKEN,
 };
+
+export default endpoints
